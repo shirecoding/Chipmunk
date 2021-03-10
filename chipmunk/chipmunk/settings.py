858de199938c98239461e4217fe41317a141bf4c
benjamin hon <benjaminhon86@gmail.com>
2021-03-10 17:42:25 +0800

    analysis and positions skeleton

--- chipmunk/chipmunk/settings.py
+++ chipmunk/chipmunk/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'positions',
+    'analysis'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'chipmunk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            Path(BASE_DIR).joinpath('templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
