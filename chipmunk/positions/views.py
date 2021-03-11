c912aff94311fcbc3261637971a8cf4286d91261
benjamin hon <benjaminhon86@gmail.com>
2021-03-11 15:57:55 +0800

    render accounts

--- chipmunk/positions/views.py
+++ chipmunk/positions/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from .models import Account, Position
 from django_pandas.io import read_frame
+
+from .models import Account, Position
 
 
 def home(request):
 
     positions = Position.objects.all()
+    accounts = Account.objects.all()
 
-    df = read_frame(positions)
-    
-    return render(request, 'positions/home.html', {'pandas_html': df.to_html()})
+    positions_df = read_frame(positions)[
+        ["name", "symbol", "account", "shares", "cost_basis"]
+    ]
+    accounts_df = read_frame(accounts)[["name", "account_type", "cash_balance"]]
+
+    return render(
+        request,
+        "positions/home.html",
+        {
+            "positions_df": positions_df.to_html(index=False),
+            "accounts_df": accounts_df.to_html(index=False),
+        },
+    )
