9e126dc049d4d7e102a618ca7986bd58af4e9a6d
benjamin hon <benjaminhon86@gmail.com>
2021-03-12 14:46:59 +0800

    add simple registration authentication

--- chipmunk/home/urls.py
+++ chipmunk/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import home
+from .views import home, login_view, register
 
 urlpatterns = [
-    path('', home, name='home')
+    path("register", register, name="register"),
+    path("login", login_view, name="login"),
+    path("", home, name="home"),
 ]
